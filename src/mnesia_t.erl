a16defa9af2a6334dfb79e37dfbb79cd37884a5e
Dan Gudmundsson <dgud@erlang.org>
2009-12-11 11:01:21 +0100

    Initial commit

---
+++ src/mnesia_t.erl
@@ -0,0 +1,205 @@
+%%%----------------------------------------------------------------------
+%%% File    : mnesia_t.erl
+%%% Author  : Dan Gudmundsson <dgud@erix.ericsson.se>
+%%% Purpose : Mnesia debugging tools
+%%% Created :  8 Oct 1999 by Dan Gudmundsson <dgud@erix.ericsson.se>
+%%%----------------------------------------------------------------------
+
+-module(mnesia_t).
+-author('dgud@erix.ericsson.se').
+
+-compile(export_all).
+%%-export([Function/Arity, ...]).
+
+
+lm() ->
+    Pids = [{P, process_info(P, messages)} 
+	    || P <- processes(),
+	       {message_queue_len, 0} < process_info(P, message_queue_len)].
+    
+
+show(X) ->
+    show(X, []).
+show(F, A) when list(F) ->
+    io:format(user, F, A);
+show(Fd, F) when pid(Fd) ->
+    io:format(Fd, F, []).
+show(File, F, A) ->
+    io:format(File, F, A).
+
+core() ->
+    Prefix = "MnesiaCore.",
+    Filter = fun(F) -> lists:prefix(Prefix, F) end,
+    {ok, Cwd} = file:get_cwd(),
+    case file:list_dir(Cwd) of
+	{ok, Files}->
+	    CoreFiles = lists:sort(lists:zf(Filter, Files)),
+	    show("Mnesia core files: ~p~n", [CoreFiles]),
+	    [core(CoreF) || CoreF <- CoreFiles];
+	Error ->
+	    Error
+    end.
+
+logfiles() ->
+    {ok, Cwd} = file:get_cwd(),
+    {ok, Files} =  file:list_dir(Cwd),
+    Filter = fun(F) ->
+		     Rev = lists:reverse(F),
+		     lists:prefix("DCD", Rev) or lists:prefix("LCD", Rev)
+	     end,
+    LogFiles = lists:zf(Filter, Files),
+    {ok, Out} = file:open("LogFiles.txt", [write]),
+    [viewlog(Out,LogF) || LogF <- LogFiles],
+    file:close(Out).
+	
+
+
+core(File) ->
+    OutFileStr = "TxT" ++ File ++ ".txt",
+    {ok, Out} = file:open(OutFileStr, [write]),
+    show("~n***** Mnesia core: ~p *****~n", [File]),
+    case file:read_file(File) of
+	{ok, Bin} ->
+	    vcore(Bin, Out);
+	_ ->
+	    show("~n No Core File..exiting~n")
+    end,
+    file:close(Out).
+
+vcore(Bin, Out) when binary(Bin) ->
+    Core = binary_to_term(Bin),
+    Fun = fun({Item, Info}) ->
+		  show(Out, "***** ~p *****~n", [Item]),
+		  case catch vcore_elem(Out, {Item, Info}) of
+		      {'EXIT', Reason} ->
+			  show(Out, "{'EXIT', ~p}~n", [Reason]);
+		      _ -> ok
+		  end
+	  end,
+    lists:foreach(Fun, Core).
+    
+vcore_elem(Fd, {schema_file, {ok, B}}) ->
+    Fname = "/tmp/schema.DAT",
+    file:write_file(Fname, B),
+%    dets:view(Fname),
+%    file:delete(Fname);
+    show(Fd, "Dets file not viewied", []);
+
+vcore_elem(Fd, {logfile, {ok, BinList}}) ->
+    Fun = fun({F, Info}) ->
+		  show(Fd,"----- logfile: ~p -----~n", [F]),
+		  case Info of
+		      {ok, B} ->
+			  Fname = "/tmp/mnesia_vcore_elem.TMP",
+			  file:write_file(Fname, B),
+			  viewlog(Fd, Fname),
+			  file:delete(Fname);
+		      _ ->
+			  show(Fd, "~p~n", [Info])
+		  end
+	  end,
+    lists:foreach(Fun, BinList);
+
+vcore_elem(Fd,{crashinfo, {Format, Args}}) ->
+    show(Fd,Format, Args);
+vcore_elem(Fd,{gvar, L}) ->
+    show(Fd, "~p~n", [lists:sort(L)]);
+vcore_elem(Fd, {transactions, Info}) ->
+    mnesia_tm:display_info(Fd, Info);
+vcore_elem(Fd, {Item, Info}) ->
+    show(Fd, "~p~n", [Info]).
+
+viewlog(Out, File) ->
+    show(Out, "*****  ~p ***** ~n", [File]),
+    case mnesia_lib:exists(File) of
+	false ->
+	    show(Out, "No such file ~p~n", [File]),
+	    nolog;
+	true ->
+	    Repair = mnesia_monitor:get_env(auto_repair),
+	    N = view_only,
+	    Args = [{file, File}, {name, N}, {repair, Repair}],
+	    case disk_log:open(Args) of
+		{ok, N} ->
+		    view_file(Out, start, N);
+		{repaired, _, _, _} ->
+		    view_file(Out, start, N);
+		{error, Reason} ->
+		    show(Out, "Cannot open log ~p: ~p~n", [File, Reason])
+	    end
+    end.
+
+view_file(Out, C, Log) ->
+    case disk_log:chunk(Log, C) of
+	{error, Reason} ->
+	    show(Out, "** Possibly truncated FILE ~n", []),
+	    error;
+	eof ->
+	    disk_log:close(Log),
+	    eof;
+	{C2, Terms} ->
+	    lists:foreach(fun(X) -> show(Out, "~p~n", [X]) end, Terms),
+	    view_file(Out, C2, Log)
+    end.
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nc() ->
+    Mods = mnesia:ms(),
+    nc(Mods).
+
+nc(Mods) when list(Mods)->
+    [Mod || Mod <- Mods, ok /= load(Mod, compile)].
+
+ni() -> 
+    Mods = mnesia:ms(),
+    ni(Mods).
+
+ni(Mods) when list(Mods) ->
+    [Mod || Mod <- Mods, ok /= load(Mod, interpret)].
+
+load(Mod, How) when atom(Mod) ->
+    case try_load(Mod, How) of
+	ok ->
+	    ok;
+	_ ->
+	    mnesia_lib:show( "~n RETRY ~p FROM: ", [Mod]),
+	    Abs = mod2abs(Mod),
+	    load(Abs, How)
+    end;
+load(Abs, How) ->
+    case try_load(Abs, How) of
+	ok ->
+	    ok;
+	{error, Reason} ->
+	    mnesia_lib:show( " *** ERROR *** ~p~n", [Reason]),
+	    {error, Reason}
+    end.
+
+try_load(Mod, How) ->
+    mnesia_lib:show( " ~p ", [Mod]),
+    Flags = [{d, debug}],
+    case How of
+	compile ->
+	    case catch c:nc(Mod, Flags) of
+		{ok, _} -> ok;
+		Other -> {error, Other}
+	    end;
+	interpret ->
+	    case catch int:ni(Mod, Flags) of
+		{module, _} -> ok;
+		Other -> {error, Other}
+	    end
+    end.
+
+mod2abs(Mod) ->
+    ModString = atom_to_list(Mod),
+    SubDir =
+	case lists:suffix("test", ModString) of
+	    true -> test;
+	    false -> src
+	end,
+    filename:join([code:lib_dir(mnesia), SubDir, ModString]).
---
+++ src/shell_default.erl
@@ -0,0 +1,163 @@
+%% ``The contents of this file are subject to the Erlang Public License,
+%% Version 1.1, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Public License along with this software. If not, it can be
+%% retrieved via the world wide web at http://www.erlang.org/.
+%% 
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%% 
+%% The Initial Developer of the Original Code is Ericsson Utvecklings AB.
+%% Portions created by Ericsson are Copyright 1999, Ericsson Utvecklings
+%% AB. All Rights Reserved.''
+%% 
+%%     $Id$
+%%
+
+%% shell_default is intended for 'rolling you own'
+%% add or remove code as you feel free
+%% This is just a empty template which calls routoines in the modulke c
+%% to do all the work!
+
+-module(shell_default).
+
+-export([help/0,lc/1,c/1,c/2,nc/1,nl/1,l/1,i/0,pid/3,i/3,m/0,m/1,
+         memory/0,memory/1,
+	 erlangrc/1,bi/1, regs/0, flush/0,pwd/0,ls/0,ls/1,cd/1, 
+	 zi/0, bt/1, q/0,
+	 ni/0, nregs/0]).
+
+-export([ih/0,iv/0,im/0,ii/1,ii/2,iq/1,ini/1,ini/2,inq/1,ib/2,ib/3,
+	 ir/2,ir/3,ibd/2,ibe/2,iba/3,ibc/3,
+	 ic/0,ir/1,ir/0,il/0,ipb/0,ipb/1,iaa/1,iaa/2,ist/1,ia/1,ia/2,ia/3,
+	 ia/4,ip/0]).
+
+%% MY own stuff !!
+-export([pi/1, pi/3, pid/1, mi/0, ti/1, p2n/0, p2nhelp/0]).
+-export([make/0]).
+
+-import(io, [format/1]).
+
+help() ->
+    format('** shell internal commands **~n'),
+    format('b()        -- display all variable bindings~n'),
+    format('e(N)       -- repeat the expression in query <N>~n'),
+    format('f()        -- forget all variable bindings~n'),
+    format('f(X)       -- forget the binding of variable X~n'),
+    format('h()        -- history~n'),    
+    format('history(N) -- set how many previous commands to keep\n'),
+    format('results(N) -- set how many previous command results to keep\n'),
+    format('v(N)       -- use the value of query <N>~n'),
+    format('** commands in module c **~n'),
+    c:help(),
+    format('** commands in module i (interpreter interface) **~n'),
+    format('ih()       -- print help for the i module~n'),
+    %% format('** private commands ** ~n'),
+    %% format('myfunc()   -- does my operation ...\n'),
+    format('~n My own stuff~n'),
+    format('pi(N | reg_name)  -- process_info(Pid)~n'),
+    format('mi()              -- mnesia:info()~n'),
+    true.
+
+
+make() ->
+    case make:all([load,debug_info]) of
+	up_to_date -> ok;
+	error -> exit(compile_failed)
+    end.	    
+
+pi(Pid)  ->
+    catch process_info(pid(Pid)).
+pi(A,B,C) ->
+    catch process_info(pid(A,B,C)).
+
+pid(Pid) when pid(Pid) ->
+    Pid;
+pid(RegName) when atom(RegName) ->
+    whereis(RegName);
+pid(N) when number(N) ->
+    c:pid(0,N,0);
+pid([A,B,C]) ->
+    c:pid(A,B,C);
+pid(Pid) when list(Pid) ->
+    erlang:list_to_pid(Pid);
+pid({A,B,C}) ->
+    c:pid(A,B,C).
+
+mi() ->
+    mnesia:info().
+
+ti(Tab) ->
+    [io:format("~p~n", [I]) || I <- mnesia:table_info(Tab, all)],
+    ok.
+
+p2n() ->
+    Ns = [node()|nodes()],
+    lists:sort(rpc:multicall(Ns, ?MODULE, p2nhelp, [])).
+
+p2nhelp() ->
+    {node(), self()}.
+
+%% these are in alphabetic order it would be nice if they
+%% were to *stay* so!
+
+bi(I) 		-> c:bi(I).
+bt(Pid)		-> c:bt(pid(Pid)).
+c(File) 	-> c:c(File).
+c(File, Opt)    -> c:c(File, Opt).
+cd(D)           -> c:cd(D).
+erlangrc(X) 	-> c:erlangrc(X).
+flush()         -> c:flush().
+i() 		-> c:i().
+i(X,Y,Z) 	-> c:i(X,Y,Z).
+l(Mod)       	-> c:l(Mod).
+lc(X)  		-> c:lc(X).
+ls()            -> c:ls().
+ls(S)           -> c:ls(S).
+m() 		-> c:m().
+m(Mod) 		-> c:m(Mod).
+memory()        -> c:memory().
+memory(Type)    -> c:memory(Type).
+nc(X)     	-> c:nc(X).
+ni()            -> c:ni().
+nl(Mod) 	-> c:nl(Mod).
+nregs()         -> c:nregs().
+pid(X,Y,Z) 	-> c:pid(X,Y,Z).
+pwd()           -> c:pwd().
+q()		-> c:q().
+regs()          -> c:regs().
+zi() 		-> c:zi().
+
+iaa(Flag)       -> i:iaa(Flag).
+iaa(Flag,Fnk)   -> i:iaa(Flag,Fnk).
+ist(Flag)       -> i:ist(Flag).
+ia(Pid)         -> i:ia(pid(Pid)).
+ia(X,Y,Z)       -> i:ia(X,Y,Z).
+ia(Pid,Fnk)     -> i:ia(pid(Pid),Fnk).
+ia(X,Y,Z,Fnk)   -> i:ia(X,Y,Z,Fnk).
+ib(Mod,Line)    -> i:ib(Mod,Line).
+ib(Mod,Fnk,Arity) -> i:ib(Mod,Fnk,Arity).
+ibd(Mod,Line)   -> i:ibd(Mod,Line).
+ibe(Mod,Line)   -> i:ibe(Mod,Line).
+iba(M,L,Action) -> i:iba(M,L,Action).
+ibc(M,L,Cond)   -> i:ibc(M,L,Cond).
+ic()            -> i:ic().
+ih()            -> i:help().
+ii(Mod)         -> i:ii(Mod).
+ii(Mod,Op)      -> i:ii(Mod,Op).
+il()            -> i:il().
+im()            -> i:im().
+ini(Mod)        -> i:ini(Mod).
+ini(Mod,Op)     -> i:ini(Mod,Op).
+inq(Mod)        -> i:inq(Mod).
+ip()            -> i:ip().
+ipb()           -> i:ipb().
+ipb(Mod)        -> i:ipb(Mod).
+iq(Mod)         -> i:iq(Mod).
+ir(Mod,Line)    -> i:ir(Mod,Line).
+ir(Mod,Fnk,Arity) -> i:ir(Mod,Fnk,Arity).
+ir(Mod)         -> i:ir(Mod).
+ir()            -> i:ir().
+iv()            -> i:iv().
